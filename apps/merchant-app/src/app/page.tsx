3931c87e35b6293b63f8f944848ff7f4feae0673
Ritik Sachan <sachanritik1@gmail.com>
2024-05-30 03:59:22 +0530

    pulled from harkirat branch

--- apps/merchant-app/src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function page() {
-  return <div className="bg-red-600">Merchant app</div>;
-}
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,16 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  return new PrismaClient()
 }
 
-const prisma: ReturnType<typeof prismaClientSingleton> =
-  globalThis.prismaGlobal ?? prismaClientSingleton();
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-export default prisma;
+const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
