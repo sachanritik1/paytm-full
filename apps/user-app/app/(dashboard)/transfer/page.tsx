b5864f1e8e62f19322c09de6cc2a2b56282c2ee8
Ritik Sachan <sachanritik1@gmail.com>
2024-05-31 19:04:04 +0530

    added prettierrc and fixed tsconfig error in db

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,41 +1,41 @@
-import prisma from "@repo/db/client";
-import { AddMoney } from "../../../components/AddMoneyCard";
-import { BalanceCard } from "../../../components/BalanceCard";
-import { OnRampTransactions } from "../../../components/OnRampTransactions";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../lib/auth";
+import prisma from '@repo/db/client'
+import { AddMoney } from '../../../components/AddMoneyCard'
+import { BalanceCard } from '../../../components/BalanceCard'
+import { OnRampTransactions } from '../../../components/OnRampTransactions'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../../lib/auth'
 
 async function getBalance() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
   const balance = await prisma.balance.findFirst({
     where: {
       userId: Number(session?.user?.id),
     },
-  });
+  })
   return {
     amount: balance?.amount || 0,
     locked: balance?.locked || 0,
-  };
+  }
 }
 
 async function getOnRampTransactions() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
   const txns = await prisma.onRampTransaction.findMany({
     where: {
       userId: Number(session?.user?.id),
     },
-  });
+  })
   return txns.map((t) => ({
     time: t.startTime,
     amount: t.amount,
     status: t.status,
     provider: t.provider,
-  }));
+  }))
 }
 
 export default async function () {
-  const balance = await getBalance();
-  const transactions = await getOnRampTransactions();
+  const balance = await getBalance()
+  const transactions = await getOnRampTransactions()
 
   return (
     <div className="w-screen">
@@ -54,5 +54,5 @@ export default async function () {
         </div>
       </div>
     </div>
-  );
+  )
 }
